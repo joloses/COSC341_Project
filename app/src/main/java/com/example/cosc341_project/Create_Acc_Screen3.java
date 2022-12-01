836029eed523947140646d4fe5e68e75083d0bcc
Brandon <bmackicloud@gmail.com>
2022-12-01 12:16:26 -0800

    Finished create page

--- app/src/main/java/com/example/cosc341_project/Create_Acc_Screen3.java
+++ app/src/main/java/com/example/cosc341_project/Create_Acc_Screen3.java
@@ -2,13 +2,58 @@ package com.example.cosc341_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class Create_Acc_Screen3 extends AppCompatActivity {
+
+    RadioButton pro1, pro2, pro3, pro4, pro5;
+    Button nextButt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_acc_screen3);
+
+        pro1 = findViewById(R.id.radButt1);
+        pro2 = findViewById(R.id.radButt2);
+        pro3 = findViewById(R.id.radButt3);
+        pro4 = findViewById(R.id.radButt4);
+        pro5 = findViewById(R.id.radButt5);
+
+        nextButt = findViewById(R.id.ca1_nxtbutt3);
+        nextButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent ca_intent = new Intent(Create_Acc_Screen3.this, Create_Acc_Screen4.class);
+                Bundle bundle = new Bundle();
+
+                if(pro1.isChecked() == true) {
+                    String s1 = (pro1.getText().toString());
+                    bundle.putString("pro1", s1);
+                } else if(pro2.isChecked() == true) {
+                    String s2 = (pro2.getText().toString());
+                    bundle.putString("pro2", s2);
+                } else if(pro3.isChecked() == true) {
+                    String s3 = (pro3.getText().toString());
+                    bundle.putString("pro3", s3);
+                } else if(pro4.isChecked() == true) {
+                    String s4 = (pro4.getText().toString());
+                    bundle.putString("pro4", s4);
+                } else if(pro5.isChecked() == true) {
+                    String s5 = (pro5.getText().toString());
+                    bundle.putString("pro5", s5);
+                }
+
+                ca_intent.putExtras(bundle);
+                startActivity(ca_intent);
+            }
+        });
     }
 }
+
+
---
+++ app/src/main/java/com/example/cosc341_project/Create_Acc_Screen4.java
@@ -0,0 +1,63 @@
+package com.example.cosc341_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Create_Acc_Screen4 extends AppCompatActivity {
+
+    CheckBox sport1, sport2, sport3, sport4;
+    EditText text1;
+    Button nextButt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_acc_screen4);
+
+        sport1 = findViewById(R.id.hockeyBox);
+        sport2 = findViewById(R.id.basketballBox);
+        sport3 = findViewById(R.id.baseballBox);
+        sport4 = findViewById(R.id.soccerBox);
+        text1 = findViewById(R.id.otherFillText);
+
+        nextButt = findViewById(R.id.ca1_nxtbutt3);
+        nextButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Create_Acc_Screen4.this, Entry_Screen.class);
+                Bundle bundle = new Bundle();
+
+                if(sport1.isChecked() == true) {
+                    String s1 = (sport1.getText().toString());
+                    bundle.putString("sport1", s1);
+                }
+                if(sport2.isChecked() == true) {
+                    String s2 = (sport2.getText().toString());
+                    bundle.putString("sport2", s2);
+                }
+                if(sport3.isChecked() == true) {
+                    String s3 = (sport3.getText().toString());
+                    bundle.putString("sport3", s3);
+                }
+                if(sport4.isChecked() == true) {
+                    String s4 = (sport4.getText().toString());
+                    bundle.putString("sport4", s4);
+                }
+
+                String s5 = text1.getText().toString();
+                bundle.putString("text1", s5);
+
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+    }
+}

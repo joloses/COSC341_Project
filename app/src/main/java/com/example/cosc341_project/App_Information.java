efe64afec9ddb659b967660f1e64b2be1b744e43
joloses <jolosesbreno@gmail.com>
2022-12-01 00:13:54 -0800

    Added Back button on About page.

--- app/src/main/java/com/example/cosc341_project/App_Information.java
+++ app/src/main/java/com/example/cosc341_project/App_Information.java
@@ -3,13 +3,23 @@ package com.example.cosc341_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class App_Information extends AppCompatActivity {
+
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_information);
+
+
+    }
+
+    public void backToSetting(View view){
+        super.onBackPressed();
     }
 
 
--- app/src/main/java/com/example/cosc341_project/Settings_Preferences.java
+++ app/src/main/java/com/example/cosc341_project/Settings_Preferences.java
@@ -91,8 +91,6 @@ public class Settings_Preferences extends AppCompatActivity {
 
 
 
-    public void toPreviousActivity(View view){
-        super.onBackPressed();
-    }
+
 
 }

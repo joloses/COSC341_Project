276ed3bab4e3e45b3f643c1b4a5ad1d688e63258
joloses <jolosesbreno@gmail.com>
2022-12-05 02:49:10 -0800

    Added Notification Switch on Preferences Page.

--- app/src/main/java/com/example/cosc341_project/Settings_Preferences.java
+++ app/src/main/java/com/example/cosc341_project/Settings_Preferences.java
@@ -2,29 +2,42 @@ package com.example.cosc341_project;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
+
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Settings_Preferences extends AppCompatActivity {
 
-    //Initialize variables
-    Button infoPage, signOut;
+    // Distance Variables
     SeekBar distanceBar;
     TextView distanceVal;
+    // Age Variables
     EditText editMinAge, editMaxAge;
+    //Spinners
     Spinner genderSpinner, levelSpinner;
+    //Chosen Variables
     int distanceChosen, minAgeChosen, maxAgeChosen;
     String genderChosen, levelChosen;
+    //Notification Variables
+    Switch notification;
+    static boolean notifOn;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -37,7 +50,7 @@ public class Settings_Preferences extends AppCompatActivity {
         distanceBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                distanceVal = (TextView) findViewById(R.id.distanceBarVal);
+                distanceVal = findViewById(R.id.distanceBarVal);
                 distanceVal.setText(i + " km");
 
                 distanceChosen = i;
@@ -69,7 +82,7 @@ public class Settings_Preferences extends AppCompatActivity {
         //Put gender choices in spinner and gather chosen option
         String[] genderArray = new String[]{"No Preference", "Male", "Female"};
         genderSpinner = findViewById(R.id.genderSpinner);
-        ArrayAdapter<String> genderAdapter = new ArrayAdapter<String>(this,
+        ArrayAdapter<String> genderAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, genderArray);
         genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         genderSpinner.setAdapter(genderAdapter);
@@ -79,7 +92,7 @@ public class Settings_Preferences extends AppCompatActivity {
         //Put level of play choices in spinner and gather chosen option
         String[] levelArray = new String[]{"Casual/Fun", "Training", "(Semi) Pro"};
         levelSpinner = findViewById(R.id.levelSpinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, levelArray);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         levelSpinner.setAdapter(adapter);
@@ -88,50 +101,78 @@ public class Settings_Preferences extends AppCompatActivity {
 
     }
 
-        //Set up information page here -> intent to new activity
-        public void toInfoContact(View v) {
-            Intent intent = new Intent(Settings_Preferences.this, App_Information.class);
-            startActivity(intent);
+
+    //Notifications Tab (Fake notification for design purposes)
+    public void toggleNotifications(View v) {
+
+        notification = findViewById(R.id.switchNotif);
+
+        if (notification.isChecked()) {
+            notifOn = true;
+            sendNotification(this, "Team Up", "Notifications is On");
+        } else {
+            notifOn = false;
+            Toast.makeText(this, "Notifications is Off", Toast.LENGTH_SHORT).show();
         }
-
-        //Set up signout page here -> intent to pop up confirming action then bring to main page (finish())
-        public void signOutConfirm(View v){
-
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-
-            alertDialogBuilder.setTitle("Confirm Sign Out");
-            alertDialogBuilder.setMessage("Are you sure,You want to sign out?");
-            alertDialogBuilder.setCancelable(false);
-
-            alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-
-                @Override
-                public void onClick(DialogInterface arg0, int arg1) {
-                    finish();
-                }
-            });
-
-            alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                }
-            });
-            alertDialogBuilder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                }
-            });
-
-            AlertDialog alertDialog = alertDialogBuilder.create();
-            alertDialog.show();
-        }
-
-    public void backToProfile(View v){
-        super.onBackPressed();
     }
 
+    public void sendNotification(Context context, String contentTitle, String contentText) {
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        if (notifOn) {
+            Notification notification = new NotificationCompat.Builder(this, notifChannel.CHANNEL_ID_1)
+                    .setContentTitle(contentTitle)
+                    .setContentText(contentText)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setCategory(NotificationCompat.CATEGORY_STATUS)
+                    .build();
+            notificationManager.notify(1, notification);
+        }
+    }
+
+
+    //Set up information page here -> intent to new activity
+    public void toInfoContact(View v) {
+        Intent intent = new Intent(Settings_Preferences.this, App_Information.class);
+        startActivity(intent);
+    }
+
+    //Set up signout page here -> intent to pop up confirming action then bring to main page (finish())
+    public void signOutConfirm(View v) {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        alertDialogBuilder.setTitle("Confirm Sign Out");
+        alertDialogBuilder.setMessage("Are you sure,You want to sign out?");
+        alertDialogBuilder.setCancelable(false);
+
+        alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                finish();
+            }
+        });
+
+        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        alertDialogBuilder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    public void backToProfile(View v) {
+        super.onBackPressed();
+    }
 
 
 }

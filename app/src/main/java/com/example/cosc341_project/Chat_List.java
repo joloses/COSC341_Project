bf8b02d7ab5c5188dd04a468fd89bc3aefb7740b
Egor <egormaxb@gmail.com>
2022-12-04 21:45:08 -0800

    Chat list design

---
+++ app/src/main/java/com/example/cosc341_project/Chat_List.java
@@ -0,0 +1,14 @@
+package com.example.cosc341_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chat_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_list);
+    }
+}
